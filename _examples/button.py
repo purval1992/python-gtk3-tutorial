24a1ed7a8102889edece387b7395f61001bfc239
Andrew Steele <andrew@andrewsteele.me.uk>
2015-10-23 20:51:35 +0100

    Clean up of Button and Switch demos

--- _examples/button.py
+++ _examples/button.py
@@ -2,16 +2,19 @@
 
 from gi.repository import Gtk
 
-def button_clicked(button):
-    print("Button has been clicked!")
+class Button(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.connect("destroy", Gtk.main_quit)
 
-window = Gtk.Window()
-window.connect("destroy", Gtk.main_quit)
+        button = Gtk.Button(label="Button")
+        button.connect("clicked", self.on_button_clicked)
+        self.add(button)
 
-button = Gtk.Button(label="Button")
-button.connect("clicked", button_clicked)
-window.add(button)
+    def on_button_clicked(self, button):
+        print("Button has been clicked!")
 
+window = Button()
 window.show_all()
 
 Gtk.main()
--- _examples/switch.py
+++ _examples/switch.py
@@ -2,19 +2,22 @@
 
 from gi.repository import Gtk
 
-def switch_toggled(switch, state):
-    if switch.get_active():
-        print("Switch toggled to on")
-    else:
-        print("Switch toggled to off")
+class Switch(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.connect("destroy", Gtk.main_quit)
 
-window = Gtk.Window()
-window.connect("destroy", Gtk.main_quit)
+        switch = Gtk.Switch()
+        switch.connect("notify::active", self.on_switch_toggled)
+        self.add(switch)
 
-switch = Gtk.Switch()
-switch.connect("notify::active", switch_toggled)
-window.add(switch)
+    def on_switch_toggled(self, switch, state):
+        if switch.get_active():
+            print("Switch toggled to on")
+        else:
+            print("Switch toggled to off")
 
+window = Switch()
 window.show_all()
 
 Gtk.main()
