6ee1c1473300f5bc699fb15d84ffd31e944a8e6d
Andrew Steele <andrew@andrewsteele.me.uk>
2015-11-18 10:24:23 +0000

    Shifted EntryBuffer example to class

--- _examples/entrybuffer.py
+++ _examples/entrybuffer.py
@@ -2,27 +2,29 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.connect("destroy", Gtk.main_quit)
+class EntryBuffer(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.connect("destroy", Gtk.main_quit)
 
-box = Gtk.Box()
-box.set_orientation(Gtk.Orientation.VERTICAL)
-box.set_spacing(5)
-window.add(box)
+        box = Gtk.Box()
+        box.set_orientation(Gtk.Orientation.VERTICAL)
+        self.add(box)
 
-entrybuffer = Gtk.EntryBuffer()
-entrybuffer.set_text("Text in EntryBuffer", -1)
+        entrybuffer = Gtk.EntryBuffer()
+        entrybuffer.set_text("Text in EntryBuffer", -1)
 
-entry = Gtk.Entry()
-entry.set_buffer(entrybuffer)
-box.pack_start(entry, True, True, 0)
-entry = Gtk.Entry()
-entry.set_buffer(entrybuffer)
-box.pack_start(entry, True, True, 0)
-entry = Gtk.Entry()
-entry.set_buffer(entrybuffer)
-box.pack_start(entry, True, True, 0)
+        entry = Gtk.Entry()
+        entry.set_buffer(entrybuffer)
+        box.pack_start(entry, True, True, 0)
+        entry = Gtk.Entry()
+        entry.set_buffer(entrybuffer)
+        box.pack_start(entry, True, True, 0)
+        entry = Gtk.Entry()
+        entry.set_buffer(entrybuffer)
+        box.pack_start(entry, True, True, 0)
 
+window = EntryBuffer()
 window.show_all()
 
 Gtk.main()
