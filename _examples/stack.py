c9be4f6ee01a1aeec9d1a15550f6201d9232c454
Andrew Steele <andrew@andrewsteele.me.uk>
2015-11-24 17:20:49 +0000

    Moved Stack-based examples to class

--- _examples/stack.py
+++ _examples/stack.py
@@ -2,35 +2,38 @@
 
 from gi.repository import Gtk
 
-def on_button_clicked(button, page):
-    name = "label%i" % page
-    stack.set_visible_child_name(name)
+class Stack(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(400, 300)
+        self.connect("destroy", Gtk.main_quit)
 
-window = Gtk.Window()
-window.set_default_size(400, 300)
-window.connect("destroy", Gtk.main_quit)
+        grid = Gtk.Grid()
+        self.add(grid)
 
-grid = Gtk.Grid()
-window.add(grid)
+        self.stack = Gtk.Stack()
+        self.stack.set_vexpand(True)
+        self.stack.set_hexpand(True)
+        grid.attach(self.stack, 0, 0, 1, 1)
 
-stack = Gtk.Stack()
-stack.set_vexpand(True)
-stack.set_hexpand(True)
-grid.attach(stack, 0, 0, 1, 1)
+        buttonbox = Gtk.ButtonBox()
+        buttonbox.set_layout(Gtk.ButtonBoxStyle.CENTER)
+        grid.attach(buttonbox, 0, 1, 1, 1)
 
-buttonbox = Gtk.ButtonBox()
-buttonbox.set_layout(Gtk.ButtonBoxStyle.START)
-grid.attach(buttonbox, 0, 1, 1, 1)
+        for page in range(1, 4):
+            label = Gtk.Label("Stack Content on Page %i" % (page))
+            name = "label%i" % page
+            self.stack.add_named(label, name)
 
-for page in range(1, 4):
-    label = Gtk.Label("Stack Content on Page %i" % (page))
-    name = "label%i" % page
-    stack.add_named(label, name)
+            button = Gtk.Button("Page %i" % (page))
+            button.connect("clicked", self.on_button_clicked, page)
+            buttonbox.add(button)
 
-    button = Gtk.Button("Page %i" % (page))
-    button.connect("clicked", on_button_clicked, page)
-    buttonbox.add(button)
+    def on_button_clicked(self, button, page):
+        name = "label%i" % page
+        self.stack.set_visible_child_name(name)
 
+window = Stack()
 window.show_all()
 
 Gtk.main()
--- _examples/stacksidebar.py
+++ _examples/stacksidebar.py
@@ -2,28 +2,31 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.set_default_size(400, 300)
-window.connect("destroy", Gtk.main_quit)
+class StackSidebar(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(400, 300)
+        self.connect("destroy", Gtk.main_quit)
 
-grid = Gtk.Grid()
-window.add(grid)
+        grid = Gtk.Grid()
+        self.add(grid)
 
-stack = Gtk.Stack()
-stack.set_hexpand(True)
-stack.set_vexpand(True)
-grid.attach(stack, 1, 0, 1, 1)
+        stack = Gtk.Stack()
+        stack.set_hexpand(True)
+        stack.set_vexpand(True)
+        grid.attach(stack, 1, 0, 1, 1)
 
-stacksidebar = Gtk.StackSidebar()
-stacksidebar.set_stack(stack)
-grid.attach(stacksidebar, 0, 0, 1, 1)
+        stacksidebar = Gtk.StackSidebar()
+        stacksidebar.set_stack(stack)
+        grid.attach(stacksidebar, 0, 0, 1, 1)
 
-for page in range(1, 4):
-    label = Gtk.Label("Stack Content on Page %i" % (page))
-    name = "label%i" % page
-    title = "Page %i" % page
-    stack.add_titled(label, name, title)
+        for page in range(1, 4):
+            label = Gtk.Label("Stack Content on Page %i" % (page))
+            name = "label%i" % page
+            title = "Page %i" % page
+            stack.add_titled(label, name, title)
 
+window = StackSidebar()
 window.show_all()
 
 Gtk.main()
