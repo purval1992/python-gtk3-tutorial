986671a422cb3354e97b69328c0c70ec05315d63
Andrew Steele <andrew@andrewsteele.me.uk>
2015-11-28 08:09:21 +0000

    Correction to Fixed container example Updated details in AboutDialog demo

--- _examples/fixed.py
+++ _examples/fixed.py
@@ -2,18 +2,21 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.set_default_size(200, 200)
-window.connect("destroy", Gtk.main_quit)
+class Fixed(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(200, 200)
+        self.connect("destroy", Gtk.main_quit)
 
-layout = Gtk.Layout()
-window.add(layout)
+        fixed = Gtk.Fixed()
+        self.add(fixed)
 
-button = Gtk.Button(label="Button 1")
-layout.put(button, 40, 60)
-button = Gtk.Button(label="Button 2")
-layout.put(button, 155, 80)
+        button = Gtk.Button(label="Button 1")
+        fixed.put(button, 40, 60)
+        button = Gtk.Button(label="Button 2")
+        fixed.put(button, 120, 95)
 
+window = Fixed()
 window.show_all()
 
 Gtk.main()
--- _examples/layout.py
+++ _examples/layout.py
@@ -2,18 +2,21 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.set_default_size(200, 200)
-window.connect("destroy", Gtk.main_quit)
+class Layout(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(200, 200)
+        self.connect("destroy", Gtk.main_quit)
 
-layout = Gtk.Layout()
-window.add(layout)
+        layout = Gtk.Layout()
+        self.add(layout)
 
-button = Gtk.Button(label="Button 1")
-layout.put(button, 40, 60)
-button = Gtk.Button(label="Button 2")
-layout.put(button, 155, 80)
+        button = Gtk.Button(label="Button 1")
+        layout.put(button, 40, 60)
+        button = Gtk.Button(label="Button 2")
+        layout.put(button, 120, 95)
 
+window = Layout()
 window.show_all()
 
 Gtk.main()
