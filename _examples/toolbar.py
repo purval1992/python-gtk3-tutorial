78aa62c92d54d66c9f9cccef3c109063e192045f
Andrew Steele <andrew@andrewsteele.me.uk>
2015-11-30 16:18:55 +0000

    Setup class for Toolbar/ToolPalette

--- _examples/toolbar.py
+++ _examples/toolbar.py
@@ -2,50 +2,55 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.set_default_size(500, -1)
-window.connect("destroy", Gtk.main_quit)
+class Toolbar(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(500, -1)
+        self.connect("destroy", Gtk.main_quit)
 
-toolbar = Gtk.Toolbar()
-window.add(toolbar)
+        toolbar = Gtk.Toolbar()
+        self.add(toolbar)
 
-toolbutton = Gtk.ToolButton()
-toolbutton.set_is_important(True)
-toolbutton.set_icon_name("gtk-new")
-toolbar.add(toolbutton)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_label("New")
+        toolbutton.set_is_important(True)
+        toolbutton.set_icon_name("gtk-new")
+        toolbar.add(toolbutton)
 
-toggletoolbutton = Gtk.ToggleToolButton()
-toggletoolbutton.set_icon_name("gtk-media-play")
-toolbar.add(toggletoolbutton)
+        toggletoolbutton = Gtk.ToggleToolButton()
+        toggletoolbutton.set_icon_name("gtk-media-play")
+        toolbar.add(toggletoolbutton)
 
-menu = Gtk.Menu()
-menuitem = Gtk.MenuItem(label="MenuItem")
-menu.append(menuitem)
+        menu = Gtk.Menu()
 
-menutoolbutton = Gtk.MenuToolButton()
-menutoolbutton.set_menu(menu)
-menutoolbutton.set_icon_name("gtk-open")
-toolbar.add(menutoolbutton)
+        menuitem = Gtk.MenuItem(label="MenuItem")
+        menu.append(menuitem)
+        menuitem.show()
 
-separatortoolitem = Gtk.SeparatorToolItem()
-toolbar.add(separatortoolitem)
+        menutoolbutton = Gtk.MenuToolButton()
+        menutoolbutton.set_menu(menu)
+        menutoolbutton.set_icon_name("gtk-open")
+        toolbar.add(menutoolbutton)
 
-radiotoolbutton1 = Gtk.RadioToolButton()
-radiotoolbutton1.set_icon_name("gtk-media-rewind")
-toolbar.add(radiotoolbutton1)
-radiotoolbutton2 = Gtk.RadioToolButton(group=radiotoolbutton1)
-radiotoolbutton2.set_icon_name("gtk-media-forward")
-toolbar.add(radiotoolbutton2)
+        separatortoolitem = Gtk.SeparatorToolItem()
+        toolbar.add(separatortoolitem)
 
-separatortoolitem = Gtk.SeparatorToolItem()
-toolbar.add(separatortoolitem)
+        radiotoolbutton1 = Gtk.RadioToolButton()
+        radiotoolbutton1.set_icon_name("gtk-media-rewind")
+        toolbar.add(radiotoolbutton1)
+        radiotoolbutton2 = Gtk.RadioToolButton(group=radiotoolbutton1)
+        radiotoolbutton2.set_icon_name("gtk-media-forward")
+        toolbar.add(radiotoolbutton2)
 
-toolitem = Gtk.ToolItem()
-entry = Gtk.Entry()
-toolitem.add(entry)
-toolbar.add(toolitem)
+        separatortoolitem = Gtk.SeparatorToolItem()
+        toolbar.add(separatortoolitem)
 
-menuitem.show()
+        toolitem = Gtk.ToolItem()
+        toolbar.add(toolitem)
+        entry = Gtk.Entry()
+        toolitem.add(entry)
+
+window = Toolbar()
 window.show_all()
 
 Gtk.main()
--- _examples/toolpalette.py
+++ _examples/toolpalette.py
@@ -2,36 +2,39 @@
 
 from gi.repository import Gtk
 
-window = Gtk.Window()
-window.set_default_size(200, 200)
-window.connect("destroy", Gtk.main_quit)
+class ToolPalette(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(200, 200)
+        self.connect("destroy", Gtk.main_quit)
 
-toolpalette = Gtk.ToolPalette()
-window.add(toolpalette)
+        toolpalette = Gtk.ToolPalette()
+        self.add(toolpalette)
 
-toolitemgroup = Gtk.ToolItemGroup(label="Group 1")
-toolpalette.add(toolitemgroup)
+        toolitemgroup = Gtk.ToolItemGroup(label="Group 1")
+        toolpalette.add(toolitemgroup)
 
-toolbutton = Gtk.ToolButton()
-toolbutton.set_icon_name("gtk-new")
-toolitemgroup.insert(toolbutton, 0)
-toolbutton = Gtk.ToolButton()
-toolbutton.set_icon_name("gtk-open")
-toolitemgroup.insert(toolbutton, 1)
-toolbutton = Gtk.ToolButton()
-toolbutton.set_icon_name("gtk-save")
-toolitemgroup.insert(toolbutton, 2)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_icon_name("gtk-new")
+        toolitemgroup.insert(toolbutton, 0)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_icon_name("gtk-open")
+        toolitemgroup.insert(toolbutton, 1)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_icon_name("gtk-save")
+        toolitemgroup.insert(toolbutton, 2)
 
-toolitemgroup = Gtk.ToolItemGroup(label="Group 2")
-toolpalette.add(toolitemgroup)
+        toolitemgroup = Gtk.ToolItemGroup(label="Group 2")
+        toolpalette.add(toolitemgroup)
 
-toolbutton = Gtk.ToolButton()
-toolbutton.set_icon_name("gtk-about")
-toolitemgroup.insert(toolbutton, 0)
-toolbutton = Gtk.ToolButton()
-toolbutton.set_icon_name("gtk-preferences")
-toolitemgroup.insert(toolbutton, 1)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_icon_name("gtk-about")
+        toolitemgroup.insert(toolbutton, 0)
+        toolbutton = Gtk.ToolButton()
+        toolbutton.set_icon_name("gtk-preferences")
+        toolitemgroup.insert(toolbutton, 1)
 
+window = ToolPalette()
 window.show_all()
 
 Gtk.main()
