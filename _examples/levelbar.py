412ddbf84c68510495d4bfbe5343f6cab53572da
Andrew Steele <andrew@andrewsteele.me.uk>
2015-12-18 19:02:20 +0000

    Moved LevelBar example to class

--- _examples/levelbar.py
+++ _examples/levelbar.py
@@ -3,16 +3,19 @@
 from gi.repository import Gtk
 import random
 
-window = Gtk.Window()
-window.set_default_size(150, -1)
-window.connect("destroy", Gtk.main_quit)
+class LevelBar(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(150, -1)
+        self.connect("destroy", Gtk.main_quit)
 
-levelbar = Gtk.LevelBar()
-levelbar.set_min_value(0)
-levelbar.set_max_value(10)
-levelbar.set_value(random.randint(0, 10))
-window.add(levelbar)
+        levelbar = Gtk.LevelBar()
+        levelbar.set_min_value(0)
+        levelbar.set_max_value(10)
+        levelbar.set_value(random.randint(0, 10))
+        self.add(levelbar)
 
+window = LevelBar()
 window.show_all()
 
 Gtk.main()
