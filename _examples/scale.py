68ae289d188991e76c60c05f839ea4fc3b20859d
Andrew Steele <andrew@andrewsteele.me.uk>
2015-12-18 19:08:35 +0000

    Cleaned up example of Scale demo

--- _examples/scale.py
+++ _examples/scale.py
@@ -3,51 +3,56 @@
 from gi.repository import Gtk
 import random
 
-def add_mark(button):
-    value = scale.get_value()
-    scale.add_mark(value, Gtk.PositionType.LEFT, "Mark")
+class Scale(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_default_size(200, 200)
+        self.connect("destroy", Gtk.main_quit)
 
-def clear_marks(button):
-    scale.clear_marks()
+        grid = Gtk.Grid()
+        self.add(grid)
 
-def scale_orientation(radiobutton):
-    if radiobutton.get_label() == "Horizontal Scale":
-        scale.set_orientation(Gtk.Orientation.HORIZONTAL)
-    else:
-        scale.set_orientation(Gtk.Orientation.VERTICAL)
+        value = random.randint(0, 100)
+        adjustment = Gtk.Adjustment(value, 0, 100, 1, 10, 0)
 
-window = Gtk.Window()
-window.set_default_size(200, 200)
-window.connect("destroy", Gtk.main_quit)
+        self.scale = Gtk.Scale(orientation=Gtk.Orientation.VERTICAL, adjustment=adjustment)
+        self.scale.set_value_pos(Gtk.PositionType.BOTTOM)
+        self.scale.set_vexpand(True)
+        self.scale.set_hexpand(True)
+        grid.attach(self.scale, 0, 0, 2, 1)
 
-grid = Gtk.Grid()
-window.add(grid)
+        buttonAdd = Gtk.Button(label="Add Mark")
+        buttonAdd.connect("clicked", self.on_add_mark_clicked)
+        grid.attach(buttonAdd, 0, 1, 1, 1)
 
-value = random.randint(0, 100)
-adjustment = Gtk.Adjustment(value, 0, 100, 1, 10, 0)
+        buttonClear = Gtk.Button(label="Clear Marks")
+        buttonClear.connect("clicked", self.on_clear_marks_clicked)
+        grid.attach(buttonClear, 1, 1, 1, 1)
 
-scale = Gtk.Scale(orientation=Gtk.Orientation.VERTICAL, adjustment=adjustment)
-scale.set_value_pos(Gtk.PositionType.BOTTOM)
-scale.set_vexpand(True)
-scale.set_hexpand(True)
-grid.attach(scale, 0, 0, 2, 1)
+        radiobuttonVertical = Gtk.RadioButton(group=None, label="Vertical Scale")
+        radiobuttonVertical.orientation = 0
+        radiobuttonVertical.connect("toggled", self.on_orientation_clicked)
+        grid.attach(radiobuttonVertical, 0, 3, 2, 1)
 
-buttonAdd = Gtk.Button(label="Add Mark")
-buttonAdd.connect("clicked", add_mark)
-grid.attach(buttonAdd, 0, 1, 1, 1)
+        radiobuttonHorizontal = Gtk.RadioButton(group=radiobuttonVertical, label="Horizontal Scale")
+        radiobuttonHorizontal.orientation = 1
+        radiobuttonHorizontal.connect("toggled", self.on_orientation_clicked)
+        grid.attach(radiobuttonHorizontal, 0, 2, 2, 1)
 
-buttonClear = Gtk.Button(label="Clear Marks")
-buttonClear.connect("clicked", clear_marks)
-grid.attach(buttonClear, 1, 1, 1, 1)
+    def on_add_mark_clicked(self, button):
+        value = self.scale.get_value()
+        self.scale.add_mark(value, Gtk.PositionType.LEFT, "Mark")
 
-radiobuttonVertical = Gtk.RadioButton(group=None, label="Vertical Scale")
-radiobuttonVertical.connect("toggled", scale_orientation)
-grid.attach(radiobuttonVertical, 0, 3, 2, 1)
+    def on_clear_marks_clicked(self, button):
+        self.scale.clear_marks()
 
-radiobuttonHorizontal = Gtk.RadioButton(group=radiobuttonVertical, label="Horizontal Scale")
-radiobuttonHorizontal.connect("toggled", scale_orientation)
-grid.attach(radiobuttonHorizontal, 0, 2, 2, 1)
+    def on_orientation_clicked(self, radiobutton):
+        if radiobutton.orientation == 0:
+            self.scale.set_orientation(Gtk.Orientation.VERTICAL)
+        else:
+            self.scale.set_orientation(Gtk.Orientation.HORIZONTAL)
 
+window = Scale()
 window.show_all()
 
 Gtk.main()
